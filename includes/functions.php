e1d37e69aa2c7d2390bf7a53de23e741403664a9
codersantosh <codersantosh@gmail.com>
2026-06-22 14:44:26 +0000

    fix(patterns-travel): add Advanced Import to recommended plugins and regenerate pot

--- includes/functions.php
+++ includes/functions.php
@@ -22,7 +22,14 @@ if ( ! function_exists( 'patterns_travel_get_recommended_plugins' ) ) :
 	 * @author     codersantosh <codersantosh@gmail.com>
 	 */
 	function patterns_travel_get_recommended_plugins() {
-		$plugins = array();
+		$plugins = array(
+			array(
+				'name'   => esc_html__( 'Advanced Import', 'patterns-travel' ), // The plugin name.
+				'slug'   => 'advanced-import', // The plugin slug (eg: gutentor is the plugin slug https://wordpress.org/plugins/gutentor).
+				'plugin' => 'advanced-import/advanced-import.php', // The plugin folder and main file.
+				'url'    => 'https://wordpress.org/plugins/advanced-import/', // The plugin url.
+			),
+		);
 
 		return apply_filters( 'patterns_travel_recommended_plugins', $plugins );
 	}
